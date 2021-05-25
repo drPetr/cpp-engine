a7e83727b1faea4132c5ddff60f2f8ca8d4f533b
drpetr <petr.timohovich@gmail.com>
2021-05-25 12:57:29 +0400

    fix: scaling

--- engine/object3d_location.cpp
+++ engine/object3d_location.cpp
@@ -3,11 +3,13 @@ namespace engine {
 
 const mat4 &object3d_location::operator()() {
     if( needUpdate ) {
-        out = mat4::translation(pos) * rot.to_mat4();
+        out = mat4::translation(pos) ;
         /* scaling */
         out.x.x *= scl.x;
         out.y.y *= scl.y;
         out.z.z *= scl.z;
+        /* rotation */
+        out *= rot.to_mat4();
         needUpdate = false;
     }
     return out;
